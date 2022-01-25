7f348d95e07ceac16b5c113d2f8527635eca4865
Luis Atala <luisatala@preyhq.com>
2022-01-25 19:31:03 -0300

    chore: fix remaining tox warnings

--- tap_zendesk_sell/client.py
+++ tap_zendesk_sell/client.py
@@ -1,5 +1,7 @@
 """Zendesk Sell Base Stream class."""
 
+from typing import Any, Dict, Iterable, Optional, Tuple, Union
+
 import basecrm
 from singer_sdk import typing as th
 from singer_sdk.streams import Stream
@@ -9,39 +11,33 @@ from singer_sdk.tap_base import Tap
 class ZendeskSellStream(Stream):
     """Zendesk Sell sync stream class."""
 
-    address_schema = th.Property(
-        "address",
-        th.ObjectType(
-            th.Property(
-                "line1",
-                th.StringType,
-                description="Line 1 of the address e.g. number, street, suite, apt #, etc.",  # noqa
-            ),
-            th.Property("city", th.StringType, description="City name."),
-            th.Property(
-                "postal_code", th.StringType, description="Zip code or equivalent."
-            ),
-            th.Property("state", th.StringType, description="State name."),
-            th.Property("country", th.StringType, description="Country name."),
+    address_type = th.ObjectType(
+        th.Property(
+            "line1",
+            th.StringType,
+            description="Line 1 of the address e.g. number, street, suite, apt #, etc.",  # noqa
         ),
-        description="Address object.",
+        th.Property("city", th.StringType, description="City name."),
+        th.Property(
+            "postal_code", th.StringType, description="Zip code or equivalent."
+        ),
+        th.Property("state", th.StringType, description="State name."),
+        th.Property("country", th.StringType, description="Country name."),
     )
 
-    custom_field_type: "dict[str, th.Property]" = {
-        "address": address_schema,
-        "bool": th.Property("bool", th.StringType),
-        "date": th.Property("date", th.StringType),
-        "datetime": th.Property("datetime", th.DateTimeType),
-        "email": th.Property("email", th.StringType),
-        "list": th.Property("list", th.StringType),
-        "multi_select_list": th.Property(
-            "multi_select_list", th.ArrayType(th.StringType)
-        ),
-        "number": th.Property("number", th.StringType),
-        "phone": th.Property("phone", th.StringType),
-        "string": th.Property("string", th.StringType),
-        "text": th.Property("text", th.StringType),
-        "url": th.Property("url", th.StringType),
+    custom_field_type: Dict[str, Any] = {
+        "address": address_type,
+        "bool": th.StringType,
+        "date": th.StringType,
+        "datetime": th.DateTimeType,
+        "email": th.StringType,
+        "list": th.StringType,
+        "multi_select_list": th.ArrayType(th.StringType),
+        "number": th.StringType,
+        "phone": th.StringType,
+        "string": th.StringType,
+        "text": th.StringType,
+        "url": th.StringType,
     }
     resource_types = {
         "deal",
@@ -73,15 +69,11 @@ class ZendeskSellStream(Stream):
                 if custom_field["name"] not in custom_fields_properties:
                     custom_fields_properties[
                         custom_field["name"]
-                    ] = self.custom_field_type[custom_field["type"]].to_dict()[
-                        custom_field["type"]
-                    ]
+                    ] = self.custom_field_type[custom_field["type"]].type_dict
                 else:
                     if (
                         custom_fields_properties[custom_field["name"]]
-                        != self.custom_field_type[custom_field["type"]].to_dict()[
-                            custom_field["type"]
-                        ]
+                        != self.custom_field_type[custom_field["type"]].type_dict
                     ):
                         raise ValueError("Custom field name conflict")
         return custom_fields_properties
@@ -90,3 +82,9 @@ class ZendeskSellStream(Stream):
         """Initialize the stream."""
         super().__init__(tap)
         self.conn = basecrm.Client(access_token=self.config.get("access_token"))
+
+    def get_records(
+        self, context: Optional[dict]
+    ) -> Iterable[Union[dict, Tuple[dict, dict]]]:
+        """Return a generator of row-type dictionary objects."""
+        pass
