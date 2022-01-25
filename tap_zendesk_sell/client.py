2456ba6768f23901b79779f54ac0264fe0f6bcbb
Luis Atala <luisatala@preyhq.com>
2022-01-25 17:43:44 -0300

    chore: refactor custom fields

--- tap_zendesk_sell/client.py
+++ tap_zendesk_sell/client.py
@@ -9,116 +9,6 @@ from singer_sdk import typing as th
 class ZendeskSellStream(Stream):
     """Zendesk Sell sync stream class."""
 
-    def _update_schema(
-        self,
-        resource_type_set: set = {
-            "deal",
-            "contact",
-            "lead",
-            "prospect_and_customer",
-        },
-    ) -> dict:
-        """Update the schema for this stream with custom fields."""
-
-        resource_types = {
-            "deal",
-            "contact",
-            "lead",
-            "prospect_and_customer",
-        }
-        assert resource_type_set.issubset(
-            resource_types
-        ), f"{resource_type_set} is not a valid resource type set"
-
-        custom_field_type = {
-            "address": "object",
-            "bool": "string",
-            "date": "string",
-            "datetime": "string",
-            "email": "string",
-            "list": "string",
-            "multi_select_list": "array",
-            "number": "string",
-            "phone": "string",
-            "string": "string",
-            "text": "string",
-            "url": "string",
-        }
-        address_type = {
-            "address_line_1": ["null", "string"],
-            "city": ["null", "string"],
-            "postal_code": ["null", "string"],
-            "state": ["null", "string"],
-            "country": ["null", "string"],
-        }
-        custom_fields_properties = {}
-        for resource_type in resource_type_set:
-            _, _, data = self.conn.http_client.get(
-                "/{resource_type}/custom_fields".format(resource_type=resource_type)
-            )
-            for custom_field in data:
-                if custom_field["type"] == "address":
-                    if not custom_fields_properties.get(custom_field["name"]):
-                        custom_fields_properties[custom_field["name"]] = {
-                            "type": ["null", "object"],
-                            "properties": address_type,
-                        }
-                    else:
-                        if (
-                            "object"
-                            not in custom_fields_properties[custom_field["name"]][
-                                "type"
-                            ]
-                        ):
-                            custom_fields_properties[custom_field["name"]][
-                                "type"
-                            ].append("object")
-                        for key in address_type:
-                            if (
-                                key
-                                not in custom_fields_properties[custom_field["name"]][
-                                    "properties"
-                                ]
-                            ):
-                                custom_fields_properties[custom_field["name"]][
-                                    "properties"
-                                ][key] = address_type[key]
-                elif custom_field["type"] == "multi_select_list":
-                    if not custom_fields_properties.get(custom_field["name"]):
-                        custom_fields_properties[custom_field["name"]] = {
-                            "type": ["null", "array"],
-                            "items": {"type": "string"},
-                        }
-                    else:
-                        if (
-                            "array"
-                            not in custom_fields_properties[custom_field["name"]][
-                                "type"
-                            ]
-                        ):
-                            custom_fields_properties[custom_field["name"]][
-                                "type"
-                            ].append("array")
-                        custom_fields_properties[custom_field["name"]]["items"] = {
-                            "type": "string"
-                        }
-                elif custom_field["type"] in custom_field_type:
-                    if not custom_fields_properties.get(custom_field["name"]):
-                        custom_fields_properties[custom_field["name"]] = {
-                            "type": ["null", custom_field_type[custom_field["type"]]],
-                        }
-                    else:
-                        if (
-                            custom_field_type[custom_field["type"]]
-                            not in custom_fields_properties[custom_field["name"]][
-                                "type"
-                            ]
-                        ):
-                            custom_fields_properties[custom_field["name"]][
-                                "type"
-                            ].append(custom_field_type[custom_field["type"]])
-        return custom_fields_properties
-
     @staticmethod
     def address_schema(
         address: str = "address", description: str = "Address object.", **kwargs
@@ -142,6 +32,60 @@ class ZendeskSellStream(Stream):
             description=description,
             **kwargs,
         )
+
+    def _update_schema(
+        self,
+        resource_type_set: set = {
+            "deal",
+            "contact",
+            "lead",
+            "prospect_and_customer",
+        },
+    ) -> dict:
+        """Update the schema for this stream with custom fields."""
+
+        resource_types = {
+            "deal",
+            "contact",
+            "lead",
+            "prospect_and_customer",
+        }
+        assert resource_type_set.issubset(
+            resource_types
+        ), f"{resource_type_set} is not a valid resource type set"
+
+        custom_field_type = {
+            "address": self.address_schema(),
+            "bool": th.Property("bool", th.BooleanType),
+            "date": th.Property("date", th.StringType),
+            "datetime": th.Property("datetime", th.DateTimeType),
+            "email": th.Property("email", th.StringType),
+            "list": th.Property("list", th.StringType),
+            "multi_select_list": th.Property(
+                "multi_select_list", th.ArrayType(th.StringType)
+            ),
+            "number": th.Property("number", th.StringType),
+            "phone": th.Property("phone", th.StringType),
+            "string": th.Property("string", th.StringType),
+            "text": th.Property("text", th.StringType),
+            "url": th.Property("url", th.StringType),
+        }
+        custom_fields_properties = {}
+        for resource_type in resource_type_set:
+            _, _, data = self.conn.http_client.get(
+                "/{resource_type}/custom_fields".format(resource_type=resource_type)
+            )
+            for custom_field in data:
+                if custom_field["name"] not in custom_fields_properties:
+                    custom_fields_properties[custom_field["name"]] = custom_field_type[
+                        custom_field["type"]
+                    ].to_dict()
+                else:
+                    raise ValueError(
+                        f"Custom field {custom_field['name']} already exists."
+                    )
+
+        return custom_fields_properties
 
     def __init__(self, tap: Tap):
         super().__init__(tap)
