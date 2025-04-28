02e8db11b47a2df6bf1e391afdc279ba01cf41bd
Luis <luisatala@preyhq.com>
2025-04-28 16:32:07 -0400

    wip

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,14 +1,12 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
 import datetime
-from os import environ
 
 from singer_sdk.testing import get_tap_test_class
 
 from tap_zendesk_sell.tap import TapZendeskSell
 
 SAMPLE_CONFIG = {
-    "access_token": environ.get("TAP_ZENDESK_SELL_ACCESS_TOKEN", "your_access_token"),
     "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
     "device_uuid": "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",  # Keep existing device_uuid
 }
